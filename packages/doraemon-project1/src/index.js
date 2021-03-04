0ea3765c70ed88c1c818cf91638289b82c0e6cb4
jiansheng.fu <jiansheng.fu@qiaofang.cn>
2021-03-04 11:06:26 +0800

    a

--- packages/doraemon-project1/src/index.js
+++ packages/doraemon-project1/src/index.js
@@ -6,7 +6,7 @@ foo();
 async function main() {
   const data = await new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ a: 111 });
+      resolve({ a: 11 });
     }, 1000);
   });
   console.log(data);
